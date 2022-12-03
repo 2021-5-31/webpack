b06ddf6e7cc81d9d9d64c1597763647504bd1c1c
csd <510232445@qq.com>
2022-12-03 17:41:22 +0800

    配置文件区分环境

---
+++ webpack.base.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const FilenameList = require('./plugins/filenameList')
+
+module.exports = {
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash:6].bundle.js'
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /main\.js$/,
+  //       use: [//可以简写成 use:['loader1','loader2']
+  //         {
+  //           loader: './loaders/loader1',//也可以写成loader1?a=b
+  //           options: {
+  //             a: 'b' //自定义key和value
+  //           }
+  //         },
+  //         {
+  //           loader: 'loader2',//也可以写成loader2?c='d'
+  //           options: {
+  //             c: 'd' //自定义key和value
+  //           }
+  //         }
+  //       ]
+  //     },//规则1
+  //     // {}规则2
+  //   ]
+  // },
+  plugins: [new FilenameList('文件名列表.txt')]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,16 @@
-const path = require('path')
-const FilenameList = require('./plugins/filenameList')
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/main.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash:6].bundle.js'
-  },
-  devtool: 'source-map',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /main\.js$/,
-  //       use: [//可以简写成 use:['loader1','loader2']
-  //         {
-  //           loader: './loaders/loader1',//也可以写成loader1?a=b
-  //           options: {
-  //             a: 'b' //自定义key和value
-  //           }
-  //         },
-  //         {
-  //           loader: 'loader2',//也可以写成loader2?c='d'
-  //           options: {
-  //             c: 'd' //自定义key和value
-  //           }
-  //         }
-  //       ]
-  //     },//规则1
-  //     // {}规则2
-  //   ]
-  // },
-  plugins: [new FilenameList('文件名列表.txt')]
+const baseConfig = require('./webpack.base')
+const devConfig = require('./webpack.dev')
+const prodConfig = require('./webpack.prod')
+module.exports = (env) => {
+  if (env === 'development') {
+    return {
+      ...baseConfig,
+      ...devConfig
+    }
+  } else if (env === 'production') {
+    return {
+      ...baseConfig,
+      ...prodConfig
+    }
+  }
 }
