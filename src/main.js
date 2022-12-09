59317f811f51bf5dba4c1809801fdb30d7f57a5c
csd <510232445@qq.com>
2022-12-09 21:04:14 +0800

    babel的使用

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import css from './css/test.scss'
+const list123 = [1,2,3]
+
+list123.forEach(item=>{
+  console.log(item)
+})
+for (const item of list123) {
+  console.log(item)
+}
+new Promise((resolve) => {
+  resolve()
+})
+class A{
+  a =1 
+  constructor(b){
+    this.b = b
+  }
+  fn(){
+    let cc = 21
+  }
+}
+console.log(list123)
